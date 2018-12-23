8c91155d5f560007499465aa673081d476d4d468
Dustin Wasserman <dustin2094@gmail.com>
2018-12-23 14:14:00 -0500

    * site.css: Updated the navbar-brand to affect only the image tags   within
    * Page.cs: Added documentation comments and column attributes
    
    * ContentItem.cs: Added column attributes

--- America_Rethink/Objects/ContentItem.cs
+++ America_Rethink/Objects/ContentItem.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -12,66 +13,77 @@ namespace America_Rethink.Models
         /// Gets or sets the ContentItem ID.
         /// </summary>
         /// <value>The Primary Key for the contentitem table.</value>
+        [Column("ContentID")]
         public string Id { get; set; }
 
         /// <summary>
         /// Gets or sets the page identifier.
         /// </summary>
         /// <value>The parent page id.</value>
+        [Column("PageID")]
         public string PageID { get; set; }
 
         /// <summary>
         /// Gets or sets the content item type.
         /// </summary>
         /// <value>The type of content.</value>
+        [Column("Type")]
         public string Type { get; set; }
 
         /// <summary>
         /// Gets or sets the image.
         /// </summary>
         /// <value>The image associated with the content item.</value>
+        [Column("ImagePath")]
         public string Image { get; set; }
 
         /// <summary>
         /// Gets or sets the title.
         /// </summary>
         /// <value>The title.</value>
+        [Column("Title")]
         public string Title { get; set; }
 
         /// <summary>
         /// Gets or sets the data.
         /// </summary>
         /// <value>The meaningful content of the content item. This is data that usually ends up on the page.</value>
+        [Column("Data")]
         public string Data { get; set; }
 
         /// <summary>
         /// Gets or sets the last updated by.
         /// </summary>
         /// <value>The person that last updated the content item.</value>
+        [Column("LastUpdatedBy")]
         public string LastUpdatedBy { get; set; }
 
         /// <summary>
         /// Gets or sets the sequence number.
         /// </summary>
         /// <value>This usually corresponds to the order of items on the page.</value>
+        [Column("Seq")]
         public int Seq { get; set; }
 
         /// <summary>
         /// Gets or sets the active from date.
         /// </summary>
         /// <value>The date that the content is supposed to start appearing.</value>
-        public DateTime ActiveFrom { get; set; }
+        [Column("ActiveFrom")]
+        public DateTime ActiveFrom { get; set; } = new DateTime(1900, 01, 01, 00, 00, 00);
 
         /// <summary>
         /// Gets or sets the active to date.
         /// </summary>
         /// <value>The date that the content is supposed to stop appearing.</value>
-        public DateTime ActiveTo { get; set; }
+        [Column("ActiveTo")]
+        public DateTime ActiveTo { get; set; } = new DateTime(1900, 01, 01, 00, 00, 00);
 
         /// <summary>
         /// Gets or sets the last updated date.
         /// </summary>
         /// <value>The date that the content item was last updated.</value>
-        public DateTime LastUpdatedDate { get; set; }
+        [Column("LastUpdatedDate")]
+        public DateTime LastUpdatedDate { get; set; } = new DateTime(1900, 01, 01, 00, 00, 00);
     }
 }
--- America_Rethink/Objects/Page.cs
+++ America_Rethink/Objects/Page.cs
@@ -1,21 +1,79 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace America_Rethink.Objects
 {
     public class Page
     {
-
+        /// <summary>
+        /// Gets or sets the page identifier.
+        /// </summary>
+        /// <value>The page identifier.</value>
+        [Column("PageID")]
         public string PageID { get; set; } = "";
-        public string PageName { get; set; } = "";
-        public string PageType { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the page name.
+        /// </summary>
+        /// <value>The page name.</value>
+        [Column("PageName")]
+        public string Name { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the page type.
+        /// </summary>
+        /// <value>The page type.</value>
+        [Column("PageType")]
+        public string Type { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>The URL.</value>
+        [Column("URL")]
         public string URL { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the last updated by.
+        /// </summary>
+        /// <value>The last updated by.</value>
+        [Column("LastUpdatedBy")]
         public string LastUpdatedBy { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the last updated date.
+        /// </summary>
+        /// <value>The last updated date.</value>
+        [Column("LastUpdatedDate")]
         public DateTime LastUpdatedDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the created by.
+        /// </summary>
+        /// <value>The created by.</value>
+        [Column("CreatedBy")]
         public string CreatedBy { get; set; } = "";
+
+        /// <summary>
+        /// Gets or sets the created date.
+        /// </summary>
+        /// <value>The created date.</value>
+        [Column("CreatedDate")]
         public DateTime CreatedDate { get; set; }
-        public DateTime ActiveFrom { get; set; } 
-        public DateTime ActiveTo { get; set; }
 
+        /// <summary>
+        /// Gets or sets the active from.
+        /// </summary>
+        /// <value>The active from.</value>
+        [Column("ActiveFrom")]
+        public DateTime ActiveFrom { get; set; } = new DateTime(1900, 01, 01, 00, 00, 00);
 
+        /// <summary>
+        /// Gets or sets the active to.
+        /// </summary>
+        /// <value>The active to.</value>
+        [Column("ActiveTo")]
+        public DateTime ActiveTo { get; set; } = new DateTime(1900, 01, 01, 00, 00, 00);
 
         public Page()
         {
@@ -24,14 +82,24 @@ namespace America_Rethink.Objects
 
         }
 
-        public override string ToString(){
+        /// <summary>
+        /// Returns a <see cref="T:System.String"/> that represents the current <see cref="T:America_Rethink.Objects.Page"/>.
+        /// </summary>
+        /// <returns>A <see cref="T:System.String"/> that represents the current <see cref="T:America_Rethink.Objects.Page"/>.</returns>
+        public override string ToString()
+        {
             string str = PageID + "    " + PageName + "    " + URL + "    " + LastUpdatedBy + "    " +
                 LastUpdatedDate.ToString() + "    " + CreatedBy + "    " + CreatedDate.ToString() + "    " + ActiveFrom.ToString() +
                                "    " + ActiveFrom.ToString() + "    " + ActiveTo.ToString() + "    " + isActive();
             return str;
         }
 
-        public bool isActive(){
+        /// <summary>
+        /// Checks to see if the current page is active.
+        /// </summary>
+        /// <returns><c>true</c>, if the current date is within the active date range, <c>false</c> if it is not within the active date range.</returns>
+        public bool IsActive()
+        {
             var now = DateTime.Now;
             var defaultDate = new DateTime(1900, 01, 01, 00, 00, 00);
 
